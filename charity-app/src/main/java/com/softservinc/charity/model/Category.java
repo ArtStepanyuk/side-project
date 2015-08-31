a88a1ee2aa4980d8d6d08aae330ac3611b5d3ff7
pavelex <pavelex@bk.ru>
2015-08-31 10:44:00 +0300

    Needs and Offers model.

--- charity-app/src/main/java/com/softservinc/charity/model/Category.java
+++ charity-app/src/main/java/com/softservinc/charity/model/Category.java
@@ -18,4 +18,24 @@ public class Category {
 
     @OneToOne
     private Category parent;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getParent() {
+        return parent;
+    }
+
+    public void setParent(Category parent) {
+        this.parent = parent;
+    }
 }
