d9cdc74935c29b508f53ad0fd599706e6e7aa1ce
pavelex <pavelex@bk.ru>
2015-09-17 12:52:54 +0300

    fix findByEmail

--- charity-app/src/main/java/com/softserveinc/charity/repository/UserRepository.java
+++ charity-app/src/main/java/com/softserveinc/charity/repository/UserRepository.java
@@ -2,9 +2,10 @@ package com.softserveinc.charity.repository;
 
 import com.softserveinc.charity.model.User;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "users", path = "users")
 public interface UserRepository extends PagingAndSortingRepository<User, Integer> {
-    User findByEmail(String email);
+    User findByEmail(@Param("email") String email);
 }
