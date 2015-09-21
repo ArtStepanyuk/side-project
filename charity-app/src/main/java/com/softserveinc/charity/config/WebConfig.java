41d8dcab239082e30cbce61603b3ae8fe58eac8c
pavelex <pavelex@bk.ru>
2015-09-21 09:25:27 +0300

    i18n + greeting controller (to check i18n use http://localhost:8080/greeting?lang=ua)

--- charity-app/src/main/java/com/softserveinc/charity/config/WebConfig.java
+++ charity-app/src/main/java/com/softserveinc/charity/config/WebConfig.java
@@ -1,9 +1,15 @@
 package com.softserveinc.charity.config;
 
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.mvc.WebContentInterceptor;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -12,38 +18,65 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @ComponentScan(basePackages = "com.softserveinc.charity.controller")
 public class WebConfig extends WebMvcConfigurerAdapter {
 
-  @Bean
-  public InternalResourceViewResolver getInternalResourceViewResolver() {
-    InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-    resolver.setPrefix("/WEB-INF/views/");
-    resolver.setSuffix(".jsp");
-    return resolver;
-  }
+    @Bean
+    public InternalResourceViewResolver getInternalResourceViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+    }
 
-  @Override
-  public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-    configurer.enable();
-  }
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+		FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		registrationBean.setFilter(characterEncodingFilter);
+		return registrationBean;
+    }
 
-  @Bean
-  public WebContentInterceptor webContentInterceptor() {
-      WebContentInterceptor interceptor = new WebContentInterceptor();
-      interceptor.setCacheSeconds(0);
-      interceptor.setUseExpiresHeader(true);
-      interceptor.setUseCacheControlHeader(true);
-      interceptor.setUseCacheControlNoStore(true);
-      return interceptor;
-  }
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+    }
 
-  @Override
-  public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//    registry.addResourceHandler("/libs/**").addResourceLocations("/libs/");
-//    registry.addResourceHandler("/app/**").addResourceLocations("/app/");
-//    registry.addResourceHandler("/assets/**").addResourceLocations("/assets/");
-  }
+    @Bean
+    public WebContentInterceptor webContentInterceptor() {
+		WebContentInterceptor interceptor = new WebContentInterceptor();
+		interceptor.setCacheSeconds(0);
+		interceptor.setUseExpiresHeader(true);
+		interceptor.setUseCacheControlHeader(true);
+		interceptor.setUseCacheControlNoStore(true);
+		return interceptor;
+    }
 
-  @Override
-  public void addInterceptors(InterceptorRegistry registry) {
-    registry.addInterceptor(webContentInterceptor());
-  }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(webContentInterceptor());
+		registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+		CookieLocaleResolver clr = new CookieLocaleResolver();
+		clr.setCookieName("lang");
+		clr.setCookieMaxAge(3600000);
+		return clr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+		lci.setParamName("lang");
+		return lci;
+    }
+
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:i18n/messages");
+		messageSource.setCacheSeconds(0);
+		messageSource.setDefaultEncoding("cp1251");
+		return messageSource;
+    }
 }
---
+++ charity-app/src/main/webapp/WEB-INF/views/greeting.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<html>
+<head>
+    <title> Serving Web Content</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</head>
+<p>Hello, ${name}<p/>
+<spring:message code="washing machine" />
+</body>
+</html>
