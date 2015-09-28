c88ba6bdc1b9dde40de2279974a38e6a0a423c11
aleksandrionov <alusbars@gmail.com>
2015-09-28 17:44:53 +0300

    Needs rest repo fix

--- charity-app/src/main/java/com/softserveinc/charity/repository/NeedRepository.java
+++ charity-app/src/main/java/com/softserveinc/charity/repository/NeedRepository.java
@@ -2,7 +2,8 @@ package com.softserveinc.charity.repository;
 
 import com.softserveinc.charity.model.Need;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@RepositoryRestResource(collectionResourceRel = "needs", path = "needs")
 public interface NeedRepository extends JpaRepository<Need, Integer> {}
