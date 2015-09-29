a1972ebd52bea6b24fb42dece954332b9ef6a47c
aleksandrionov <alusbars@gmail.com>
2015-09-29 12:52:57 +0300

    Hide need search repo

--- charity-app/src/main/java/com/softserveinc/charity/repository/NeedRepository.java
+++ charity-app/src/main/java/com/softserveinc/charity/repository/NeedRepository.java
@@ -3,7 +3,6 @@ package com.softserveinc.charity.repository;
 import com.softserveinc.charity.model.Need;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.stereotype.Repository;
 
 @RepositoryRestResource(collectionResourceRel = "needs", path = "needs")
 public interface NeedRepository extends JpaRepository<Need, Integer> {}
--- charity-app/src/main/java/com/softserveinc/charity/repository/search/NeedSearchRepository.java
+++ charity-app/src/main/java/com/softserveinc/charity/repository/search/NeedSearchRepository.java
@@ -1,17 +1,15 @@
 package com.softserveinc.charity.repository.search;
 
 import com.softserveinc.charity.model.Need;
-import org.elasticsearch.bootstrap.Elasticsearch;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
-import javax.annotation.Resource;
 import java.util.List;
 
+@RestResource(exported = false)
 public interface NeedSearchRepository
-        extends ElasticsearchRepository<Need, Integer>
+        extends ElasticsearchRepository<Need, String>
 {
     List<Need> findByName(String name);
     List<Need> findByName(String name, Pageable pageable);
