522a1297ca5b1f33a41d20cd998bd57ecf4e0c9d
aleksandrionov <alusbars@gmail.com>
2015-10-06 14:58:15 +0300

    Fix

--- charity-app/src/main/java/com/softserveinc/charity/repository/projection/OfferInLine.java
+++ charity-app/src/main/java/com/softserveinc/charity/repository/projection/OfferInLine.java
@@ -1,7 +1,6 @@
 package com.softserveinc.charity.repository.projection;
 
-import com.softserveinc.charity.model.NeedResponse;
-import com.softserveinc.charity.model.Offer;
+import com.softserveinc.charity.model.offer.Offer;
 import org.springframework.data.rest.core.config.Projection;
 
 @Projection(name = "inLine", types = {Offer.class})
